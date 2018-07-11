494a84c19b60eec71d0ed4c1a2a138d6292ff806
Abhra Chaudhuri <rick.abhra@gmail.com>
2018-07-11 18:44:47 +0530

    Update run_face_detector.py

--- run_face_detector.py
+++ run_face_detector.py
@@ -1,6 +1,6 @@
 # Program for extracting faces from a given image dataset
 # Run the program as:
-# $ python run_mser_detector.py -split train/test
+# $ python run_mser_detector.py -split Train/Test
 # If no argument is given, "train" will be taken by default
 
 from face_detector_facenet import *
@@ -17,8 +17,8 @@ def extract_faces_from_class(data_path, image_class, destination_path, split):
 	images = glob.glob(data_path + image_class + '/*')
 	# Iterate over all the images in the given class
 	for (i, im) in enumerate(images):
-		# Convert each image to RGB and resize to (224, 224)
-		im = cv2.resize(np.array(Image.open(im).convert('RGB')), (224, 224))
+		# Convert each image to RGB
+		im = np.array(Image.open(im).convert('RGB'))
 		# Get image containing only the faces
 		im = detect_faces_facenet(im)
 		if im != []:
